780aa2aa809330c73d5f308b8f74a510b740532a
Shai Mann-Robison <mann.robison@gmail.com>
2025-08-25 17:38:30 -0400

    update tab bar to have more things

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,9 @@
+import Logo from "@/assets/images/DDlogo.png";
 import { CluePath } from "@/components/CluePath";
 import { Text } from "@/components/ui/text";
+import { BookOpen } from "lucide-react-native";
 import React from "react";
-import { View } from "react-native";
+import { Image, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export interface Clue {
@@ -97,12 +99,20 @@ export default function HomePage() {
   return (
     <SafeAreaView className="flex-1 bg-white flex flex-col">
       <View className="px-5 py-2 flex flex-col items-center justify-center border-secondary border-b">
+        {/* Logo, floating left */}
+        <Image source={Logo} className="absolute left-5 pb-2 size-14" />
+
         <Text variant="h1" className="text-gray-800 font-semibold">
-          Bits&apos; Hunt
+          Bits&apos; Hunt!
         </Text>
-        <Text variant="h4" className="text-gray-500">
+        <Text variant="default" className="text-gray-500 font-semibold">
           Rank: 1
         </Text>
+
+        {/* Rules button, floating right */}
+        <TouchableOpacity className="absolute right-5 mb-2 p-2">
+          <BookOpen size={20} color="#AD8AD1" />
+        </TouchableOpacity>
       </View>
       <CluePath clues={CLUES} />
     </SafeAreaView>
