cdc2d348c5c8fe4f6f58067b0f1b693e69114a61
Shai Mann-Robison <mann.robison@gmail.com>
2025-09-03 12:57:18 -0400

    remove cors

--- server/api/health.ts
+++ server/api/health.ts
@@ -1,11 +1,6 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export = async function handler(req: VercelRequest, res: VercelResponse) {
-  // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
