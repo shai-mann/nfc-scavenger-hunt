a29bcccc22a75b2da3297b139eef0e11222986e0
Shai Mann-Robison <mann.robison@gmail.com>
2025-09-03 13:07:57 -0400

    remove extra cors headers

--- server/api/clues/[id]/unlock.ts
+++ server/api/clues/[id]/unlock.ts
@@ -4,15 +4,6 @@ import { supabase } from "../../../lib/supabase";
 import { ClueParamsSchema, CompleteClueSchema } from "../../../lib/types";
 
 export = async function handler(req: VercelRequest, res: VercelResponse) {
-  // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
   if (req.method !== "POST") {
     return res.status(405).json({
       success: false,
